7baadf6baf2102f8aff84d8d90bb1f3fed423ee6
CompleXss <max.petrov.27@mail.ru>
2023-05-01 18:31:31 +0300

    make nav panel responsive on mobile

--- reactapp/src/components/NavPanel/navPanel.tsx
+++ reactapp/src/components/NavPanel/navPanel.tsx
@@ -2,12 +2,29 @@ import { NavLink } from "react-router-dom";
 import './navPanel.css'
 
 export default function NavPanel() {
+    function openBurger() {
+        let nav = document.getElementById('nav');
+        if (nav === null) return;
+
+        nav.className = nav.className === 'closed'
+            ? 'open'
+            : 'closed'
+    }
+
+    function hideBurger() {
+        let nav = document.getElementById('nav');
+        if (nav === null) return;
+
+        nav.className = 'closed'
+    }
+
     return (
-        <nav>
-            <NavLink to={'/'}> Home </NavLink>
-            <NavLink to={'/history'}> History </NavLink>
-            <NavLink to={'/leaderboard'}> Leaderboard </NavLink>
-            <NavLink to={'/about'}> About </NavLink>
+        <nav id="nav" className="closed">
+            <NavLink to={'/'} onClick={hideBurger}> Home </NavLink>
+            <NavLink to={'/history'} onClick={hideBurger}> History </NavLink>
+            <NavLink to={'/leaderboard'} onClick={hideBurger}> Leaderboard </NavLink>
+            <NavLink to={'/about'} onClick={hideBurger}> About </NavLink>
+            <button className="burger" onClick={openBurger}></button>
         </nav>
     )
 }
