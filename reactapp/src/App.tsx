028573653448f9ff17ec5414cbfa8fef7dc22e4b
CompleXss <max.petrov.27@mail.ru>
2023-05-05 01:11:01 +0300

    add login page

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/home';
+import Login from './components/Login/login';
 import NavPanel from './components/NavPanel/navPanel';
 import History from './components/History/history';
 import Leaderboard from './components/LeaderBoard/leaderboard';
@@ -12,24 +13,23 @@ import './App.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className='wrapper'>
-        <NavPanel />
+    <div className='wrapper'>
+      <NavPanel />
 
-        <main>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/history' element={<History />} />
-            <Route path='/leaderboard' element={<Leaderboard />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/lobby/checkers' element={<CheckersLobby />} />
-            <Route path='/play/checkers' element={<CheckersGame />} />
-            <Route path='*' element={<Navigate to={'/'} replace={true} />} />
-          </Routes>
-        </main>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/history' element={<History />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/lobby/checkers' element={<CheckersLobby />} />
+          <Route path='/play/checkers' element={<CheckersGame />} />
+          <Route path='*' element={<Navigate to={'/'} replace={true} />} />
+        </Routes>
+      </main>
 
-        <Footer />
-      </div>
-    </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
---
+++ reactapp/src/components/Login/login.tsx
@@ -0,0 +1,110 @@
+import { useState } from 'react'
+import './login.css'
+
+export default function Login() {
+    let [switchModeText, setswitchModeText] = useState('Нет аккаунта? Регистрируйся')
+    let [isLogin, setIsLogin] = useState(true)
+
+    function switchMode() {
+        let loginBtn = document.getElementById('loginBtn')
+        let registerBtn = document.getElementById('registerBtn')
+        let loginHeader = document.getElementById('loginHeader')
+        let registerHeader = document.getElementById('registerHeader')
+
+        if (!loginBtn || !registerBtn || !loginHeader || !registerHeader) {
+            console.error('Не найдены нужные элементы на странице логина!')
+            return
+        }
+        showWarningText(false)
+
+        if (isLogin) {
+            showBtn(loginBtn, false)
+            showBtn(registerBtn, true)
+
+            showHeader(loginHeader, false)
+            showHeader(registerHeader, true)
+
+            setswitchModeText('Всё-таки есть аккаунт?')
+        }
+        else {
+            showBtn(loginBtn, true)
+            showBtn(registerBtn, false)
+
+            showHeader(loginHeader, true)
+            showHeader(registerHeader, false)
+
+            setswitchModeText('Нет аккаунта? Регистрируйся')
+        }
+
+        setIsLogin(!isLogin)
+    }
+
+    return <div id="loginWrapper">
+        <div id='loginHeaderWrapper'>
+            <h1 id='dummyHeader'>dummy</h1>
+            <h1 id='loginHeader'>Авторизация</h1>
+            <h1 id='registerHeader'>Регистрация</h1>
+        </div>
+        <div className='inputContainer'>
+            <input id='loginName' name='name' type='text' placeholder='Имя пользователя' maxLength={32} />
+            <input id='loginPassword' name='password' type='password' placeholder='Пароль' maxLength={64} />
+            <p id='loginWarningText' hidden>Введите имя и пароль</p>
+            <div className='btnContainer'>
+                <button id='dummyBtn'>dummy</button>
+                <button id='loginBtn' onClick={() => getInputAnd(login)}>Войти</button>
+                <button id='registerBtn' onClick={() => getInputAnd(register)}>Зарегистрироваться</button>
+            </div>
+        </div>
+
+        <p id='loginSwitchMode' onClick={switchMode}>{switchModeText}</p>
+    </div>
+}
+
+function getInputAnd(func: Function) {
+    let loginName = document.getElementById('loginName') as HTMLInputElement
+    let loginPassword = document.getElementById('loginPassword') as HTMLInputElement
+    if (loginName === null || loginPassword === null) return
+
+    let login = loginName.value.trim()
+    let password = loginPassword.value.trim()
+
+    if (login === '' || password === '') {
+        showWarningText(true)
+        return
+    }
+    else showWarningText(false)
+
+    func(login, password);
+}
+
+function login(login: string, password: string) {
+    console.log('login ' + login)
+}
+
+function register(login: string, password: string) {
+    console.log('register ' + login)
+}
+
+
+
+function showBtn(button: HTMLElement, show: boolean) {
+    button.style.opacity = show ? '1' : '0'
+    button.style.marginRight = show ? '16px' : '60%'
+    button.style.zIndex = show ? '0' : '-1'
+}
+
+function showHeader(header: HTMLElement, show: boolean) {
+    header.style.opacity = show ? '1' : '0'
+    header.style.marginRight = show ? '0' : '30%'
+    header.style.zIndex = show ? '0' : '-1'
+}
+
+function showWarningText(show: boolean) {
+    let text = document.getElementById('loginWarningText')
+    if (!text) {
+        console.log('Не могу найти элемент loginWarningText')
+        return
+    }
+
+    text.hidden = !show;
+}
