4ccb50185f53a6f381370f61ec07ce862aa876f4
CompleXss <max.petrov.27@mail.ru>
2023-05-22 23:56:09 +0300

    create about page

--- reactapp/src/components/About/about.tsx
+++ reactapp/src/components/About/about.tsx
@@ -6,9 +6,11 @@ export default function About() {
         document.title = 'О создателе'
     }, [])
 
-    return (
-        <h1>
-            About page text here!
-        </h1>
-    )
+    return <div className='aboutContainer'>
+        <h1> Этот сайт — курсовая работа студента 3 курса под ником </h1>
+        <h1 className='nick'>CompleX</h1>
+        <hr />
+        <a href='https://github.com/CompleXss'>Github</a>
+        <a href='https://github.com/CompleXss/WebBoardGames'>Этот сайт на том же Github</a>
+    </div>
 }
