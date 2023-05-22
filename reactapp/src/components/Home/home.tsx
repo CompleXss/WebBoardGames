aaafad89834bfded4599076ee06fcaa06a1ad1ff
CompleXss <max.petrov.27@mail.ru>
2023-05-23 02:19:32 +0300

    remove old code

--- reactapp/src/components/Home/home.tsx
+++ reactapp/src/components/Home/home.tsx
@@ -1,122 +1,27 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
-import ENDPOINTS from '../../utilities/Api_Endpoints'
 import './home.css'
 
-const CHECKERS_DEFAULT_URL = '/play/checkers'
-const CHESS_DEFAULT_URL = '/'
-const MONOPOLY_DEFAULT_URL = '/'
-
-interface ActiveGame {
-    GameType: string,
-    ID: string
-}
-
 export default function Home() {
-    const [checkersUrl, setCheckersUrl] = useState('/')
-    const [chessUrl, setChessUrl] = useState('/')
-    const [monopolyUrl, setMonopolyUrl] = useState('/')
-
     useEffect(() => {
         document.title = 'Главная'
     }, [])
 
-    // useEffect(() => {
-    //     const fetchData = () => {
-    //         axios.get(ENDPOINTS.GET_ACTIVE_GAME_URL)
-    //             .then(response => response.status === 200 ? response.data : null)
-    //             .then(json => {
-    //                 setDefaultLinks()
+    return <div className="home">
+        <h1>Во что сыграем сегодня?</h1>
 
-    //                 let activeGame = json as ActiveGame
-    //                 if (activeGame === null) { // response is not ok
-    //                     return
-    //                 }
+        <div className="gamesContainer">
+            <NavLink className="game checkers" to={'/lobby/checkers'}>
+                <p>Шашки</p>
+            </NavLink>
 
-    //                 switch (activeGame.GameType) {
-    //                     case 'checkers':
-    //                         setCheckersIDLink(activeGame.ID)
-    //                         break;
-    //                     case 'chess':
-    //                         setChessIDLink(activeGame.ID)
-    //                         break;
-    //                     case 'monopoly':
-    //                         setMonopolyIDLink(activeGame.ID)
-    //                         break;
+            <NavLink className="game chess" to={'/'}>
+                <p>Шахматы</p>
+            </NavLink>
 
-    //                     default:
-    //                         break;
-    //                 }
-    //             })
-    //             .catch((err) => {
-    //                 setDefaultLinks()
-    //                 console.log(err)
-    //             })
-    //     }
-
-    //     setEmptyLinks()
-    //     fetchData()
-    // }, [])
-
-    return (
-        <div className="home">
-            <h1>Во что сыграем сегодня?</h1>
-
-            <div className="gamesContainer">
-                <NavLink className="game checkers" to={'/lobby/checkers'}>
-                    <p>Шашки</p>
-                </NavLink>
-
-                <NavLink className="game chess" to={chessUrl}>
-                    <p>Шахматы</p>
-                </NavLink>
-
-                <NavLink className="game monopoly" to={monopolyUrl}>
-                    <p>Монополия</p>
-                </NavLink>
-            </div>
-
-            <br />
-            <h1>Статистика?</h1>
-
-            <p>11111111111</p>
-            <p>22222222222</p>
-            <p>33333333333</p>
-            <p>44444444444</p>
-            <p>55555555555</p>
-            <p>66666666666</p>
-            <p>77777777775</p>
-            <p>asdadasdasd</p>
-            <p>asdadasdasd</p>
-            <p>asdadasdasd</p>
-            <p>asdadasdasd</p>
-            <p>asdadasdasd</p>
-            <p>asdadasdasd</p>
-            <p>asdadasdasd</p>
-            <p>last</p>
+            <NavLink className="game monopoly" to={'/'}>
+                <p>Монополия</p>
+            </NavLink>
         </div>
-    )
-
-    function setEmptyLinks() {
-        setCheckersUrl('/')
-        setChessUrl('/')
-        setMonopolyUrl('/')
-    }
-
-    function setDefaultLinks() {
-        setCheckersUrl(CHECKERS_DEFAULT_URL)
-        setChessUrl(CHESS_DEFAULT_URL)
-        setMonopolyUrl(MONOPOLY_DEFAULT_URL)
-    }
-
-    function setCheckersIDLink(gameID: string) {
-        setCheckersUrl(CHECKERS_DEFAULT_URL + '/' + gameID)
-    }
-    function setChessIDLink(gameID: string) {
-        setChessUrl(CHESS_DEFAULT_URL + '/' + gameID)
-    }
-    function setMonopolyIDLink(gameID: string) {
-        setMonopolyUrl(MONOPOLY_DEFAULT_URL + '/' + gameID)
-    }
+    </div>
 }
