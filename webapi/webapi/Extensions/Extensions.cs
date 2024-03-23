c7db2d25df8b2fc4444ddb503834eac607319656
CompleXss <max.petrov.27@mail.ru>
2024-03-23 22:07:23 +0300

    Move file to extensions folder

---
+++ webapi/webapi/Extensions/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi.Extensions;
+
+public static class Extensions
+{
+    public static bool NextBoolean(this Random random)
+    {
+        // Next() returns an int in the range [0 .. int.MaxValue]
+        return random.Next() > int.MaxValue / 2;
+    }
+}
