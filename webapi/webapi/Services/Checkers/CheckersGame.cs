08529bb32831e98e7cd781b65061008608e4697d
CompleXss <max.petrov.27@mail.ru>
2024-03-23 21:18:11 +0300

    Use tabs

--- webapi/webapi/Services/Checkers/CheckersGame.cs
+++ webapi/webapi/Services/Checkers/CheckersGame.cs
@@ -4,196 +4,196 @@ namespace webapi.Services.Checkers;
 
 public sealed class CheckersGame : IDisposable
 {
-    private static readonly HashSet<string> activeKeys = new();
+	private static readonly HashSet<string> activeKeys = new();
 
-    public string Key { get; }
+	public string Key { get; }
 
-    public long WhitePlayerID { get; init; }
-    public long BlackPlayerID { get; init; }
+	public long WhitePlayerID { get; init; }
+	public long BlackPlayerID { get; init; }
 
-    public int PlayersAlive { get; set; }
-    public List<string> ConnectionIDs { get; } = new();
+	public int PlayersAlive { get; set; }
+	public List<string> ConnectionIDs { get; } = new();
 
-    public bool IsWhiteTurn { get; set; } = true;
-    public long? WinnerID { get; private set; }
+	public bool IsWhiteTurn { get; set; } = true;
+	public long? WinnerID { get; private set; }
 
-    public DateTime GameStarted { get; }
+	public DateTime GameStarted { get; }
 
-    public CheckersCell[,] Board { get; } = new CheckersCell[8, 8];
+	public CheckersCell[,] Board { get; } = new CheckersCell[8, 8];
 
-    private CheckersGame(long whitePlayerID, long blackPlayerID)
-    {
-        do
-        {
-            Key = Guid.NewGuid().ToString();
-        }
-        while (!activeKeys.Add(Key));
+	private CheckersGame(long whitePlayerID, long blackPlayerID)
+	{
+		do
+		{
+			Key = Guid.NewGuid().ToString();
+		}
+		while (!activeKeys.Add(Key));
 
-        WhitePlayerID = whitePlayerID;
-        BlackPlayerID = blackPlayerID;
+		WhitePlayerID = whitePlayerID;
+		BlackPlayerID = blackPlayerID;
 
-        GameStarted = DateTime.UtcNow;
-    }
+		GameStarted = DateTime.UtcNow;
+	}
 
-    public static CheckersGame CreateNew(long whitePlayerID, long blackPlayerID)
-    {
-        var game = new CheckersGame(whitePlayerID, blackPlayerID);
-        var board = game.Board;
+	public static CheckersGame CreateNew(long whitePlayerID, long blackPlayerID)
+	{
+		var game = new CheckersGame(whitePlayerID, blackPlayerID);
+		var board = game.Board;
 
-        // Do not change 'whites at the bottom, blacks at the top' start state!
+		// Do not change 'whites at the bottom, blacks at the top' start state!
 
-        // white
-        for (int i = 0; i < 3; i++)
-            for (int j = i & 1; j < 8; j += 2)
-            {
-                board[j, i] = new CheckersCell(CheckersCellStates.White, false);
-            }
+		// white
+		for (int i = 0; i < 3; i++)
+			for (int j = i & 1; j < 8; j += 2)
+			{
+				board[j, i] = new CheckersCell(CheckersCellStates.White, false);
+			}
 
-        // black
-        for (int i = 5; i < 8; i++)
-            for (int j = i & 1; j < 8; j += 2)
-            {
-                board[j, i] = new CheckersCell(CheckersCellStates.Black, false);
-            }
+		// black
+		for (int i = 5; i < 8; i++)
+			for (int j = i & 1; j < 8; j += 2)
+			{
+				board[j, i] = new CheckersCell(CheckersCellStates.Black, false);
+			}
 
-        return game;
-    }
+		return game;
+	}
 
-    public CheckersCellStates GetUserColor(long userID)
-    {
-        return BlackPlayerID == userID
-            ? CheckersCellStates.Black
-            : CheckersCellStates.White;
-    }
-
-
-
-    public (Draught[] myDraughts, Draught[] enemyDraughts) GetDraughtsRelativeTo(CheckersCellStates playerColor)
-    {
-        if (playerColor == CheckersCellStates.None)
-            return (Array.Empty<Draught>(), Array.Empty<Draught>());
-
-        var myList = new List<Draught>();
-        var enemyList = new List<Draught>();
-
-        var enemyColor = playerColor == CheckersCellStates.Black ? CheckersCellStates.White : CheckersCellStates.Black;
-        bool reverse = ShouldMirrorMove(playerColor);
-
-        int len1 = Board.GetLength(0);
-        int len2 = Board.GetLength(1);
-
-        for (int x = 0; x < len1; x++)
-            for (int y = 0; y < len2; y++)
-                if (Board[x, y].DraughtColor == playerColor)
-                {
-                    if (reverse)
-                        myList.Add(new Draught(7 - x, 7 - y, Board[x, y].IsQueen));
-                    else
-                        myList.Add(new Draught(x, y, Board[x, y].IsQueen));
-                }
-                else if (Board[x, y].DraughtColor == enemyColor)
-                {
-                    if (reverse)
-                        enemyList.Add(new Draught(7 - x, 7 - y, Board[x, y].IsQueen));
-                    else
-                        enemyList.Add(new Draught(x, y, Board[x, y].IsQueen));
-                }
-
-        return (myList.ToArray(), enemyList.ToArray());
-    }
+	public CheckersCellStates GetUserColor(long userID)
+	{
+		return BlackPlayerID == userID
+			? CheckersCellStates.Black
+			: CheckersCellStates.White;
+	}
 
 
 
-    public void ApplyMoves(CheckersMove[] moves)
-    {
-        foreach (var move in moves)
-        {
-            ApplyMove(Board, move);
-        }
+	public (Draught[] myDraughts, Draught[] enemyDraughts) GetDraughtsRelativeTo(CheckersCellStates playerColor)
+	{
+		if (playerColor == CheckersCellStates.None)
+			return (Array.Empty<Draught>(), Array.Empty<Draught>());
 
-        IsWhiteTurn = !IsWhiteTurn;
+		var myList = new List<Draught>();
+		var enemyList = new List<Draught>();
 
-        int whiteCount = CountCellWithState(CheckersCellStates.White);
-        if (whiteCount == 0)
-        {
-            WinnerID = BlackPlayerID;
-            return;
-        }
+		var enemyColor = playerColor == CheckersCellStates.Black ? CheckersCellStates.White : CheckersCellStates.Black;
+		bool reverse = ShouldMirrorMove(playerColor);
 
-        int blackCount = CountCellWithState(CheckersCellStates.Black);
-        if (blackCount == 0)
-        {
-            WinnerID = WhitePlayerID;
-            return;
-        }
-    }
+		int len1 = Board.GetLength(0);
+		int len2 = Board.GetLength(1);
 
-    public static void ApplyMove(CheckersCell[,] board, CheckersMove move)
-    {
-        var userColor = board[move.From.X, move.From.Y].DraughtColor;
+		for (int x = 0; x < len1; x++)
+			for (int y = 0; y < len2; y++)
+				if (Board[x, y].DraughtColor == playerColor)
+				{
+					if (reverse)
+						myList.Add(new Draught(7 - x, 7 - y, Board[x, y].IsQueen));
+					else
+						myList.Add(new Draught(x, y, Board[x, y].IsQueen));
+				}
+				else if (Board[x, y].DraughtColor == enemyColor)
+				{
+					if (reverse)
+						enemyList.Add(new Draught(7 - x, 7 - y, Board[x, y].IsQueen));
+					else
+						enemyList.Add(new Draught(x, y, Board[x, y].IsQueen));
+				}
 
-        bool isQueen =
-                board[move.From.X, move.From.Y].IsQueen
-                || userColor == CheckersCellStates.White && move.To.Y == 7
-                || userColor == CheckersCellStates.Black && move.To.Y == 0;
-
-        board[move.To.X, move.To.Y] = new CheckersCell(userColor, isQueen);
-
-        // Clear cells
-        int cellsToClearCount = Math.Abs(move.To.X - move.From.X);
-        int moveVectorX = Math.Sign(move.To.X - move.From.X);
-        int moveVectorY = Math.Sign(move.To.Y - move.From.Y);
-
-        for (int i = 0; i < cellsToClearCount; i++)
-        {
-            board[move.From.X + moveVectorX * i, move.From.Y + moveVectorY * i]
-                = new CheckersCell(CheckersCellStates.None, false);
-        }
-    }
+		return (myList.ToArray(), enemyList.ToArray());
+	}
 
 
 
-    public void DerelatifyMoves(CheckersMove[] moves, CheckersCellStates playerColor)
-    {
-        if (!ShouldMirrorMove(playerColor))
-            return;
+	public void ApplyMoves(CheckersMove[] moves)
+	{
+		foreach (var move in moves)
+		{
+			ApplyMove(Board, move);
+		}
 
-        for (int i = 0; i < moves.Length; i++)
-        {
-            var from = moves[i].From;
-            from.X = 7 - from.X;
-            from.Y = 7 - from.Y;
+		IsWhiteTurn = !IsWhiteTurn;
 
-            var to = moves[i].To;
-            to.X = 7 - to.X;
-            to.Y = 7 - to.Y;
+		int whiteCount = CountCellWithState(CheckersCellStates.White);
+		if (whiteCount == 0)
+		{
+			WinnerID = BlackPlayerID;
+			return;
+		}
 
-            moves[i] = new CheckersMove(from, to);
-        }
-    }
+		int blackCount = CountCellWithState(CheckersCellStates.Black);
+		if (blackCount == 0)
+		{
+			WinnerID = WhitePlayerID;
+			return;
+		}
+	}
+
+	public static void ApplyMove(CheckersCell[,] board, CheckersMove move)
+	{
+		var userColor = board[move.From.X, move.From.Y].DraughtColor;
+
+		bool isQueen =
+				board[move.From.X, move.From.Y].IsQueen
+				|| userColor == CheckersCellStates.White && move.To.Y == 7
+				|| userColor == CheckersCellStates.Black && move.To.Y == 0;
+
+		board[move.To.X, move.To.Y] = new CheckersCell(userColor, isQueen);
+
+		// Clear cells
+		int cellsToClearCount = Math.Abs(move.To.X - move.From.X);
+		int moveVectorX = Math.Sign(move.To.X - move.From.X);
+		int moveVectorY = Math.Sign(move.To.Y - move.From.Y);
+
+		for (int i = 0; i < cellsToClearCount; i++)
+		{
+			board[move.From.X + moveVectorX * i, move.From.Y + moveVectorY * i]
+				= new CheckersCell(CheckersCellStates.None, false);
+		}
+	}
 
 
 
-    private int CountCellWithState(CheckersCellStates state)
-    {
-        int count = 0;
+	public void DerelatifyMoves(CheckersMove[] moves, CheckersCellStates playerColor)
+	{
+		if (!ShouldMirrorMove(playerColor))
+			return;
 
-        foreach (var cell in Board)
-        {
-            if (cell.DraughtColor == state)
-                count++;
-        }
+		for (int i = 0; i < moves.Length; i++)
+		{
+			var from = moves[i].From;
+			from.X = 7 - from.X;
+			from.Y = 7 - from.Y;
 
-        return count;
-    }
+			var to = moves[i].To;
+			to.X = 7 - to.X;
+			to.Y = 7 - to.Y;
 
-    private static bool ShouldMirrorMove(CheckersCellStates playerColor)
-    {
-        return playerColor == CheckersCellStates.Black;
-    }
+			moves[i] = new CheckersMove(from, to);
+		}
+	}
 
-    public void Dispose()
-    {
-        activeKeys.Remove(Key);
-    }
+
+
+	private int CountCellWithState(CheckersCellStates state)
+	{
+		int count = 0;
+
+		foreach (var cell in Board)
+		{
+			if (cell.DraughtColor == state)
+				count++;
+		}
+
+		return count;
+	}
+
+	private static bool ShouldMirrorMove(CheckersCellStates playerColor)
+	{
+		return playerColor == CheckersCellStates.Black;
+	}
+
+	public void Dispose()
+	{
+		activeKeys.Remove(Key);
+	}
 }
