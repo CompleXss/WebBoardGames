1c57bc7e60e09f71fad103eca645ae5ea38088a3
CompleXss <max.petrov.27@mail.ru>
2024-03-30 18:02:16 +0300

    use tabs

--- webapi/webapi/Extensions/Extensions.cs
+++ webapi/webapi/Extensions/Extensions.cs
@@ -2,9 +2,9 @@
 
 public static class Extensions
 {
-    public static bool NextBoolean(this Random random)
-    {
-        // Next() returns an int in the range [0 .. int.MaxValue]
-        return random.Next() > int.MaxValue / 2;
-    }
+	public static bool NextBoolean(this Random random)
+	{
+		// Next() returns an int in the range [0 .. int.MaxValue]
+		return random.Next() > int.MaxValue / 2;
+	}
 }
