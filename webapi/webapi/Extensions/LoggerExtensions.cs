d2d1cadceac6da1c2cd78706cd09e639515c75a2
CompleXss <max.petrov.27@mail.ru>
2024-05-21 20:51:27 +0300

    Make database check log more specific

--- webapi/webapi/Extensions/LoggerExtensions.cs
+++ webapi/webapi/Extensions/LoggerExtensions.cs
@@ -29,6 +29,6 @@ public static partial class LoggerExtensions
 	[LoggerMessage(EventId = 104, Level = LogLevel.Information, Message = "Created new default value in DbSet '{dbSetName}': '{value}'")]
 	public static partial void CreatedNewDefaultEntry(this ILogger logger, string dbSetName, string value);
 
-	[LoggerMessage(EventId = 105, Level = LogLevel.Warning, Message = "App does not check whether database model is correct. Besides tables, database contains triggers so please check if schema is correct beforehand")]
+	[LoggerMessage(EventId = 105, Level = LogLevel.Warning, Message = "App does not check whether database model is correct. Database may contain triggers which are not restored if not present so please check if schema is correct beforehand. All schemas are located in Data/Schemas folder")]
 	public static partial void AppDoesNotKnowIfDatabaseSchemaIsCorrect(this ILogger logger);
 }
